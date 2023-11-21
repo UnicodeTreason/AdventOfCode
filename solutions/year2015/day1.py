4d8aefa3e24d71ee0f1219e158191ea4337398d3
UnicodeTreason <14937219+UnicodeTreason@users.noreply.github.com>
2023-11-21 20:06:59 +0800

    [year2015/day1] Complete

--- solutions/year2015/day1.py
+++ solutions/year2015/day1.py
@@ -1,10 +1,31 @@
-def calculate(inputdata):
+from pathlib import Path
+
+
+def calculate(inputData, magic=False):
     # ( means go up a floor
     # ) means go down a floor
-    output_floor = 0
-    for char in inputdata:
+    outputFloor = 0
+
+    for index, char in enumerate(inputData):
         if char == '(':
-            output_floor += 1
+            outputFloor += 1
         elif char == ')':
-            output_floor -= 1
-    return output_floor
+            outputFloor -= 1
+
+        # Part 2 Switch
+        # Calculate position in data when basement first seen
+        if magic:
+            if outputFloor <= -1:
+                # Add 1 to account for arrays starting at 0
+                return index + 1
+    return outputFloor
+
+
+if __name__ == "__main__":
+    p = Path(__file__).with_name('day1_input.txt')
+    with p.open('r') as f:
+        inputData = f.read()
+
+    print('Calculating Solutions...')
+    print(f'Solution 01: {calculate(inputData)}')
+    print(f'Solution 02: {calculate(inputData,True)}')
